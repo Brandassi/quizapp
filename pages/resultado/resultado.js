6749a9e332bb24e9aa7e91afaccd6ed4c9a4fb1f
gustavo brandassi <gustavobrandassi@icloud.com>
2024-06-13 21:51:31 -0300

    adicionada a função de jogar novamente e finalização do projeto

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,43 +1,51 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
 
-const botaoTema = document.querySelector(".tema button");
-
-const body = document.querySelector("body");
-const assunto = localStorage.getItem("assunto");
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
+const botaoJogarNovamente = document.querySelector("main button")
 
 botaoTema.addEventListener("click", () => {
-  trocarTema(body, botaoTema);
-});
+    trocarTema(body, botaoTema)
+})
 
-verificarTema(body, botaoTema);
+botaoJogarNovamente.addEventListener("click", jogarNovamente)
 
+verificarTema(body, botaoTema)
 
 function alterarAssunto() {
-  const divIcone = document.querySelector(".assunto_icone");
-  const iconeImg = document.querySelector(".assunto_icone img");
-  const assuntoTitulo = document.querySelector(".assunto h1");
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
 
-  divIcone.classList.add(assunto.toLocaleLowerCase());
-  iconeImg.setAttribute(
-    "src",
-    `../../assets/images/icon-${assunto.toLowerCase}.svg`
-  );
-  iconeImg.setAttribute("alt"`icone de ${assunto}`);
-  assuntoTitulo.innerText = assunto;
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `ícone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+
 }
 
 alterarAssunto()
 
-function inserirResultado(){
-  const sectionPontuacao = document.querySelector(".pontuacao")
-  const divAssunto = document.querySelector(".assunto")
-  const pontos = localStorage.getItem("pontos")
+function inserirResultado() {
+    const sectionPontuacao = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
 
-  sectionPontuacao.innerHTML = `
-  ${divAssunto.outerHTML}
+    sectionPontuacao.innerHTML = `
+        ${divAssunto.outerHTML}
 
-      <strong>${pontos}</strong>
+        <strong>${pontos}</strong>
 
-      <p> de 10</p>
-      `
+        <p>de 10</p>
+    `
 }
+
+function jogarNovamente() {
+    localStorage.removeItem("pontos")
+    localStorage.removeItem("assunto")
+
+    window.location.href = "../../index.html"
+}
+
+inserirResultado()
